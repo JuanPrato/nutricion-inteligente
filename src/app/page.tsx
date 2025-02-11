79975f5bed4d90ea9aa38cc10c1c4fd39bb320b9
Juan Prato <pratojuanmanuel2@gmail.com>
2025-02-11 00:47:42 -0300

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from "~/server/auth";
+import { HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <HydrateClient>
+      <main className="">
+      </main>
+    </HydrateClient>
+  );
+}
