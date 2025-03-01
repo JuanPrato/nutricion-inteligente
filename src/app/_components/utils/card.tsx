72d556eec2ae8942ba8c2fa8b589d18829f146d0
Juan Prato <pratojuanmanuel2@gmail.com>
2025-03-01 16:02:30 -0300

    add card cpm styles

--- src/app/_components/utils/card.tsx
+++ src/app/_components/utils/card.tsx
@@ -1,5 +1,6 @@
 import type { IconType } from "react-icons";
 import { twMerge } from "tailwind-merge";
+import type { ReactNode } from "react";
 
 interface LittleCardProps {
   principal: string;
@@ -7,21 +8,28 @@ interface LittleCardProps {
   top?: string;
   icon?: IconType;
   iconClassName?: string;
+  action?: ReactNode;
+  small?: boolean;
 }
 
 export function LittleCard(props: LittleCardProps) {
   return (
     <div className="little-card">
       {props.icon && (
-        <div className="bg-primary-dark flex aspect-square h-full min-h-[75px] items-center justify-center rounded-full">
+        <div className={twMerge(
+            "bg-primary-dark flex aspect-square h-full items-center justify-center rounded-full"
+        )}>
           {<props.icon className={twMerge("aspect-square size-[75%]", props.iconClassName)} />}
         </div>
       )}
-      <div className="grow flex flex-col">
-        <p className="text-lg">{props.top}</p>
-        <h3 className="text-2xl">{props.principal}</h3>
-        <h5 className="text-sm text-gray-200">{props.description}</h5>
+      <div className="grow h-full flex flex-col justify-center leading-none">
+        <p className="text-md">{props.top}</p>
+        <h3 className="text-xl">{props.principal}</h3>
+        <h5 className="text-sm text-primary">{props.description}</h5>
       </div>
+      {
+        props.action && (props.action)
+      }
     </div>
   );
 }
