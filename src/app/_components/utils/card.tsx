2cb4b616c89c6285e761ecc4f1097143abc17720
Juan Prato <pratojuanmanuel2@gmail.com>
2025-03-07 00:24:04 -0300

    big card components

--- src/app/_components/utils/card.tsx
+++ src/app/_components/utils/card.tsx
@@ -1,6 +1,6 @@
 import type { IconType } from "react-icons";
 import { twMerge } from "tailwind-merge";
-import type { ReactNode } from "react";
+import type { PropsWithChildren, ReactNode } from "react";
 
 interface LittleCardProps {
   principal: string;
@@ -17,21 +17,66 @@ export function LittleCard(props: LittleCardProps) {
     <div className="little-card">
       {props.icon && (
         <div className="bg-primary-dark flex aspect-square h-full items-center justify-center rounded-full">
-          {<props.icon className={twMerge("aspect-square size-[75%]", props.iconClassName)} />}
+          {
+            <props.icon
+              className={twMerge(
+                "aspect-square size-[75%]",
+                props.iconClassName,
+              )}
+            />
+          }
         </div>
       )}
-      <div className="w-full h-full flex flex-col justify-center leading-none">
+      <div className="flex h-full w-full flex-col justify-center leading-none">
         <p className="text-md">{props.top}</p>
         <h3 className="text-xl">{props.principal}</h3>
-        <h5 className="text-sm text-primary">{props.description}</h5>
+        <h5 className="text-primary text-sm">{props.description}</h5>
       </div>
-      {
-        props.action && (
-            <div className="w-[75px]">
-              {props.action}
-            </div>
-          )
-      }
+      {props.action && <div className="w-[75px]">{props.action}</div>}
     </div>
   );
+}
+
+export function Card(_: PropsWithChildren) {
+  return <div className="card">{_.children}</div>;
+}
+
+interface CardHeaderProps {
+  title: string;
+  subtitle?: string;
+  icon?: IconType;
+}
+
+export function CardHeader(props: CardHeaderProps) {
+  return (
+    <header className="card-header">
+      <div className="card-top">
+        <h2>{props.title}</h2>
+        {props.subtitle && <h4>{props.subtitle}</h4>}
+      </div>
+      {props.icon && (
+        <div className="icon-container">
+          {
+            <props.icon className={twMerge("size-[65%]")} />
+          }
+        </div>
+      )}
+    </header>
+  );
+}
+
+export function CardContent(props: PropsWithChildren) {
+  return (
+      <div className="card-content">
+        {props.children}
+      </div>
+  );
+}
+
+export function CardActions(props: PropsWithChildren) {
+  return (
+      <div className="card-actions">
+        {props.children}
+      </div>
+  )
 }
