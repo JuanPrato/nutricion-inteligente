030584366e55727a3d03c36b39d0a14aa9dd2edc
Juan Prato <pratojuanmanuel2@gmail.com>
2025-03-07 17:56:26 -0300

    profile information styles

--- src/app/_components/profile/ProfileInformation.tsx
+++ src/app/_components/profile/ProfileInformation.tsx
@@ -13,9 +13,17 @@ export async function ProfileInformation() {
     redirect(ROUTES.LOGIN);
   }
 
+  const user = session.user;
+
   return (
       <Section title="Información personal">
-        <Avatar img={session.user.image} alt="profile img" />
+        <div className="flex gap-4 justify-center">
+          <Avatar img={user.image} alt="profile img" className="size-24 outline"  />
+          <div className="flex flex-col justify-center">
+            <h3 className="text-xl font-semibold">{user.name}</h3>
+            <h4 className="font-light text-gray-200">{user.email}</h4>
+          </div>
+        </div>
       </Section>
   );
 }
--- src/app/_components/utils/avatar.tsx
+++ src/app/_components/utils/avatar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 interface AvatarProps {
   img?: string | null | undefined;
   alt?: string;
+  className?: string;
 }
 
 export function Avatar(props: AvatarProps) {
   return (
-      <div className="aspect-square bg-primary-dark rounded-full relative w-1/3 overflow-hidden">
+      <div className={twMerge("aspect-square bg-primary-dark rounded-full relative w-1/3 overflow-hidden", props.className)}>
         <Image src={props.img ?? ""} alt={props.alt ?? ""} fill></Image>
       </div>
   );
