603eb8054e50f66fccbdbe5082b678c2de00f6b7
Juan Prato <pratojuanmanuel2@gmail.com>
2025-03-07 00:24:31 -0300

    section action

--- src/app/_components/utils/section.tsx
+++ src/app/_components/utils/section.tsx
@@ -3,13 +3,17 @@ import { twMerge } from "tailwind-merge";
 
 interface Props {
   title?: string;
+  action?: React.ReactNode;
   className?: string;
 }
 
 export function Section(props: PropsWithChildren<Props>) {
   return (
       <section className={twMerge("w-full flex flex-col items-start gap-2 p-4 bg-primary-darker shadow-lg rounded-lg border", props.className)}>
-        {props.title && <h2 className="text-2xl font-bold">{props.title}</h2>}
+        <header className="w-full flex items-center">
+          {props.title && <h2 className="text-2xl font-bold grow">{props.title}</h2>}
+          {props.action}
+        </header>
         {props.children}
       </section>
   );
