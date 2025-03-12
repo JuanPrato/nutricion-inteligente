1ff49f76ac86c1f2751e4c515fbe9da540dee413
Juan Prato <pratojuanmanuel2@gmail.com>
2025-03-12 00:07:34 -0300

    add hover layar to compatibility

--- src/app/_components/utils/button.tsx
+++ src/app/_components/utils/button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
@@ -23,21 +23,27 @@ interface LinkButtonProps extends Omit<ButtonProps, "onClick"> {
 
 export function Button(props: ButtonProps) {
   return (
-      <button
-        className={twMerge(
-          "cursor-pointer flex items-center gap-3 p-2 rounded-md transition-[backdrop-filter]",
-          props.selected && "bg-primary-dark",
-          !props.selected && "hover:backdrop-brightness-90",
-          props.full ? "w-full" : "w-2/3",
-          props.circle && "rounded-full",
-          props.outlined && "outline outline-primary",
-          props.className
-        )}
-        onClick={props.onClick}
-      >
-        {props.left} {props.children && <span className={twMerge("text-lg", props.textClassName)}>{props.children}</span>}
-      </button>
-  )
+    <button
+      className={twMerge(
+        "relative flex cursor-pointer items-center gap-3 rounded-md p-2 overflow-hidden",
+        props.selected && "bg-primary-dark",
+        props.full ? "w-full" : "w-2/3",
+        props.circle && "rounded-full",
+        props.outlined && "outline-primary outline",
+        props.className,
+      )}
+      onClick={props.onClick}
+      type="button"
+    >
+      <div className={twJoin("absolute top-0 left-0 h-full w-full bg-white opacity-0", !props.selected && "hover:opacity-25")}></div>
+      {props.left}{" "}
+      {props.children && (
+        <span className={twMerge("text-lg", props.textClassName)}>
+          {props.children}
+        </span>
+      )}
+    </button>
+  );
 }
 
 
