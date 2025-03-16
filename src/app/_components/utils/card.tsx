ab9b99a6502ebbd1cef4ffd95d52089c473a0ee3
Juan Prato <pratojuanmanuel2@gmail.com>
2025-03-16 11:35:47 -0300

    add small little card variant

--- src/app/_components/utils/card.tsx
+++ src/app/_components/utils/card.tsx
@@ -14,7 +14,7 @@ interface LittleCardProps {
 
 export function LittleCard(props: LittleCardProps) {
   return (
-    <div className="little-card">
+    <div className={twMerge("little-card", props.small && "little-card-small")}>
       {props.icon && (
         <div className="bg-primary-dark flex aspect-square h-full items-center justify-center rounded-full">
           {
@@ -27,12 +27,12 @@ export function LittleCard(props: LittleCardProps) {
           }
         </div>
       )}
-      <div className="flex h-full w-full flex-col justify-center leading-none">
+      <div className={twMerge("flex h-full w-full flex-col justify-center leading-none", props.small && "h-fit flex-row justify-start items-baseline")}>
         <p className="text-md">{props.top}</p>
         <h3 className="text-xl">{props.principal}</h3>
-        <h5 className="text-primary text-sm">{props.description}</h5>
+        <h5 className={twMerge("text-primary text-sm", props.small && "text-muted ml-1 text-xs")}>{props.description}</h5>
       </div>
-      {props.action && <div className="w-[75px]">{props.action}</div>}
+      {props.action && <div className="w-fit">{props.action}</div>}
     </div>
   );
 }
